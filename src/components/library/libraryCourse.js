a4306690f017195d8d02d4e6a534ac858483dab7
Torick Davis <torickjdavis@gmail.com>
2018-07-02 15:04:45 +0000

    Library course component Grid finished, and created Action component.

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Icon from '../icon';
 import Arrow from '../arrow';
+import Action from '../action';
 
 class LibraryCourse extends Component {
 	render() {
@@ -11,8 +12,8 @@ class LibraryCourse extends Component {
 					<label className="title">Problem Solving</label>
 					{ Icon('fas fa-check')}
 				</header>
-				{/* arrow component*/}
 				<Arrow />
+				<Action />
 				<div className="description">
 					<label>Course Description</label>
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque quis accumsan enim. Suspendisse potenti. Pellentesque ultricies sit amet diam id vulputate. Sed sit amet imperdiet mauris. Nunc convallis, erat a imperdiet porttitor, velit ex fringilla neque, eget interdum tortor arcu sit amet lacus. Phasellus eleifend odio nec mauris dictum, et eleifend ex lobortis.</p>
