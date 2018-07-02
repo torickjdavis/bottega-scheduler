785bb2fc4e443fefed6c573031b5b7f304a52b32
Torick Davis <torickjdavis@gmail.com>
2018-07-02 04:39:43 +0000

    Added Arrow component, and adjusted grid and hierarchy.

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 
 import Icon from '../icon';
+import Arrow from '../arrow';
 
 class LibraryCourse extends Component {
 	render() {
 		return (
 			<div className="course">
-				<label className="title">Problem Solving</label>
-				{ Icon('fas fa-check')}
+				<header>
+					<label className="title">Problem Solving</label>
+					{ Icon('fas fa-check')}
+				</header>
 				{/* arrow component*/}
-				{/* action button component*/}
+				<Arrow />
 				<div className="description">
 					<label>Course Description</label>
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque quis accumsan enim. Suspendisse potenti. Pellentesque ultricies sit amet diam id vulputate. Sed sit amet imperdiet mauris. Nunc convallis, erat a imperdiet porttitor, velit ex fringilla neque, eget interdum tortor arcu sit amet lacus. Phasellus eleifend odio nec mauris dictum, et eleifend ex lobortis.</p>
