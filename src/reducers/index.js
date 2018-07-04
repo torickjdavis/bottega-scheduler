ec12cb25af4729298c869758bcfe701c7ea72c52
Torick Davis <torickjdavis@gmail.com>
2018-07-04 21:59:48 +0000

    Built coursesReducer and connected Library component to Redux.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
+import courses from './coursesReducer';
+
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+	courses
 });
 
 export default rootReducer;
