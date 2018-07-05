797cba39c3780e4eb26a3a60a9161606b2c1195d
Torick Davis <torickjdavis@gmail.com>
2018-07-05 15:31:59 +0000

    Finished functionality

--- src/components/schedule/progressTracker.js
+++ src/components/schedule/progressTracker.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 
 class ProgressTracker extends Component {
+	calculateProgress = function() {
+		return (this.props.amountEnrolled / this.props.amountPossible) * 100;
+	}.bind(this);
+
 	render() {
 		return (
 			<div className="progressTracker">
 				<label>Progress Tracker</label>
-				<div className="percentage">0%</div>
+				<div className="percentage">{this.calculateProgress()}%</div>
 			</div>
 		);
 	}
--- src/components/schedule/schedule.js
+++ src/components/schedule/schedule.js
@@ -9,18 +9,20 @@ class Schedule extends Component {
 	renderCourses() {
 		const data = this.props.courses;
 		return data.map((course, index) => {
-			if (course.enrolled) {
+			if (course.enrolled && this.amountEnrolled < 5) {
+				this.amountEnrolled++;
 				return <ScheduleCourse {...course} key={index} />;
 			}
 		});
 	}
 
 	render() {
+		this.amountEnrolled = 0;
 		return (
 			<div className="schedule">
 				<h1>My Schedule</h1>
 				{ this.renderCourses() }
-				<ProgressTracker />
+				<ProgressTracker amountPossible={5} amountEnrolled={this.amountEnrolled}/>
 				<Gradient />
 			</div>
 		);
