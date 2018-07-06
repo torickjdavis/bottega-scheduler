3bd8ada9458048547cba5b8af0dd6a372945f2b3
Torick Davis <torickjdavis@gmail.com>
2018-07-06 20:48:40 +0000

    Styled action button. (Add/Remove)

--- src/components/action.js
+++ src/components/action.js
@@ -3,14 +3,23 @@ import React, { Component } from 'react';
 import Icon from './icon';
 
 class Action extends Component {
+	handleAction = function() {
+		this.props.onClick();
+	}.bind(this);
+
 	render() {
 		return (
-			<button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
-				{ Icon('fas fa-plus-circle', 'action') }
-				{/* fas fa-times-circle */}
-			</button>
+			<a onClick={() => this.handleAction() } className={`${this.props.className} action`}>
+			</a>
 		);
 	}
 }
 
 export default Action;
+
+// return (
+// 			<button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
+// 				{ Icon('fas fa-plus-circle', 'action') }
+// 				{/* fas fa-times-circle */}
+// 			</button>
+// 		);
