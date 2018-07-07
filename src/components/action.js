f532f05014e0d7feaf6255ccbe87758216abae6a
Torick Davis <torickjdavis@gmail.com>
2018-07-07 05:45:05 +0000

    Further styled, animated, and added functionality to action for remove and adding courses.

--- src/components/action.js
+++ src/components/action.js
@@ -3,13 +3,22 @@ import React, { Component } from 'react';
 import Icon from './icon';
 
 class Action extends Component {
-	handleAction = function() {
+	constructor(props) {
+		super(props);
+
+		this.status = false;
+	}
+
+	handleAction = function(e) {
 		this.props.onClick();
+		let action = e.target;
+		action.classList.toggle('remove'); // uses classes rather than ID's
+		this.status = !this.status; // not needed, but kept
 	}.bind(this);
 
 	render() {
 		return (
-			<a onClick={() => this.handleAction() } className={`${this.props.className} action`}>
+			<a onClick={(e) => this.handleAction(e) } className={`${this.props.className} action`}>
 			</a>
 		);
 	}
