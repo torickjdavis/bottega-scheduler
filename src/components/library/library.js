617a49748ec5427f4af0133dc6ce8552a9d99598
Torick Davis <torickjdavis@gmail.com>
2018-07-07 23:50:53 +0000

    Overflow scrolling on the Library component.

--- src/components/library/library.js
+++ src/components/library/library.js
@@ -20,8 +20,10 @@ class Library extends Component {
 	render() {
 		return (
 			<div className="library">
-				<h1>Course Library</h1>
-				{ this.renderCourses() }
+				<div className="container">
+					<h1>Course Library</h1>
+					{ this.renderCourses() }
+				</div>
 			</div>
 		);
 	}
